aa08ae45747579d75027472770195d183f2a5e21
Shir Hershkovich <shither0203@gmail.com>
2025-08-03 18:01:36 +0300

    Add Group model with validation and schema structure

---
+++ models/Group.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Group', groupSchema);
