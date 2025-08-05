f7e30a2d7af3473d64002b3b686a659b6e8f0fa5
Shir Hershkovich <shither0203@gmail.com>
2025-08-05 11:29:41 +0300

    add middleware that can check before action if the user in the session in Deleted and return error if it is

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,18 @@ const isAuthenticated = (req, res, next) => {
   if (req.session && req.session.userId) {
     return next();
   } else {
-    return res.status(401).json({ message: 'Unauthorized: Please log in' });
+    return res.status(401).json({ message: "Unauthorized: Please log in" });
   }
 };
 
-module.exports = { isAuthenticated };
+const checkUserNotDeleted = async (req, res, next) => {
+  const user = await User.findById(req.session.userId);
+  if (!user || user.isDeleted) {
+    return res
+      .status(403)
+      .json({ message: "User account is deleted or inactive" });
+  }
+  next();
+};
 
+module.exports = { isAuthenticated, checkUserNotDeleted };
