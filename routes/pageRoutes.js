bd8cdab0ce1c4e76d7e3be73fef24a76c08da6b7
Shir Hershkovich <shither0203@gmail.com>
2025-08-05 13:52:47 +0300

    add page routes

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middleware/authMiddleware');
 
+// redirect root "/" to login or feed
+router.get('/', (req, res) => {
+  if (req.session && req.session.userId) {
+    return res.redirect('/feed');
+  } else {
+    return res.redirect('/login');
+  }
+});
+
 // Render login page
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// Render register page
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
 // Render feed page
-router.get('/feed', isAuthenticated, (req, res) => {
-  res.render('feed'); // this looks for views/feed.ejs
+router.get('/feed', (req, res) => {
+  res.render('feed');
 });
 
 module.exports = router;
