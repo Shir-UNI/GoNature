cf97428570736c1192387963c2a7a36f34ad1dc2
Shir Hershkovich <shither0203@gmail.com>
2025-08-05 16:13:05 +0300

    support default profile picture

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     },
     profileImage: {
       type: String,
-      default: "/images/default-profile.png",
+      default: "/public/uploads/profiles/default.png",
     },
     isDeleted: {
       type: Boolean,
